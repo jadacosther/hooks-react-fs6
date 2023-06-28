f27bb93224d2e914abe7555ee5b18d4edb71053d
jadacosther <123274200+jadacosther@users.noreply.github.com>
2023-06-27 21:49:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jadacosther.github.io/hooks-react-fs6.git"
+  base: "https://jadacosther.github.io/hooks-react-fs6"
 })
